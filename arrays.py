35b13229af0627655744bf226193e41f1acc2088
Ayiekoo <131998667+Ayiekoo@users.noreply.github.com>
2023-05-28 21:05:01 +0300

    Add files via upload

--- arrays.py
+++ arrays.py
@@ -1,206 +1,135 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+## COMMON MATHEMATICAL AND STATISTICAL FUNCTIONS IN NUMPY
+import numpy as np
+
+
 # In[2]:
 
 
-import numpy as np
+arr = np.array([[5,3,5],[10,0,1],[8,5,4]])
+print(arr)
 
 
 # In[3]:
 
 
-np.array(24) 
+## minimum value in the array
+np.min(arr)
+
+"""
+gives the minimum value in the array is axis is not passed
+"""
 
 
 # In[4]:
 
 
-np.array([1,2,3,4])
+## we can obtain the axis of the array
+np.min(arr,axis=0) 
+
+"""
+This code gives the minimum value in each axis//the colum and row
+"""
 
 
 # In[5]:
 
 
-np.array([[1,1,1],[1,1,1]])  ### this is a 2D array
-
-
-# In[7]:
-
-
-np.array([[[1,1,1],[2,2,2],[3,3,3],[4,4,4]]])  ## this is a 3D array
+np.max(arr) ## gives the maximum value in the array if axis is not passed
 
 
 # In[8]:
 
 
-numpy_arr = np.array([[[1,1,1],[2,2,2],[3,3,3],[4,4,4]]]) 
+### lets pass the axis
+np.max(arr,axis=1)
+
+"""
+Obtains the maximum value in axis=1
+"""
 
 
 # In[9]:
 
 
-numpy_arr.shape ### obtains the shape of the array created above
+### standard deviations
+
+
+### median
+np.median(arr)
 
 
 # In[10]:
 
 
-numpy_arr.ndim  ## obtains the dimensions of the array
+np.mean(arr)  ## mean of the array
 
 
 # In[11]:
 
 
-numpy_arr = np.array([[1, 2, 3], [4, 5, 6]])
-
-print(numpy_arr.ndim) ##### A SHORTER CODE FOR PRINTING THE DIMENSION OF ARRAYS
+np.std(arr)  ## standard deviation of the array
 
 
 # In[12]:
 
 
-numpy_arr = np.array([[[1,1,1],[2,2,2],[3,3,3],[4,4,4]]]) 
-print(numpy_arr.ndim)  ## prints the output of the array
-                        ### is a shorter version of the code above
+np.var(arr)  ### variance of the array
 
 
 # In[13]:
 
 
-numpy_arr = np.array([x for x in range(1,10)])
-print(numpy_arr)
+### PERCENTILES
+
+np.percentile(arr,50)
+
+## obtains the percentile of the array after the 50th percentile
+
+
+# In[14]:
+
+
+deg = np.array([6,30,45,60,90])  ## this produces an output of degreees
+
+
+# In[15]:
+
+
+np.sin(deg*np.pi/100)
 
 
 # In[16]:
 
 
-## convert to 3D array
-numpy_arr.reshape(3,3)
+## we can manipulate the cos and sin
+np.cos(deg*np.pi/100)
+
+
+# In[17]:
+
+
+np.tan(deg*np.pi/100)
+
+
+# In[18]:
+
+
+arr = np.array([0.1,0.8,-2.2,-9.8])
+np.floor(arr)
+
+## output is the floor of the values
 
 
 # In[19]:
 
 
-## convert to 1 to 3D array
-numpy_arr = np.array([x for x in range(1,13)])
-
-numpy_arr.reshape(2,2,3)
-
-
-# In[21]:
-
-
-numpy_arr.reshape(3,2,2)
-
-
-# In[22]:
-
-
-numpy_arr = numpy_arr.reshape(3,2,2)
-print(numpy_arr)
-
-
-# In[24]:
-
-
-### we can flatten the array using the reshape
-numpy_arr.reshape(-1)  ## flattens the array 
-
-
-# In[25]:
-
-
-#### ADDING ARRAYS
-
-A = np.array([[1,2,1],[3,3,2]])
-B = np.array([7,8,9])
-
-
-# In[26]:
-
-
-print(A)
-
-
-# In[27]:
-
-
-print(B)
-
-
-# In[28]:
-
-
-np.add(A,B) ## ADDITION OF ARRAYS
-
-
-# In[29]:
-
-
-np.subtract(B,A) ### SUBTRACTION OF ARRAYS
-
-
-# In[30]:
-
-
-np.multiply(A,B) ### MULTIPLICATION OF A&B
-
-
-# In[31]:
-
-
-np.divide(A,B)  ### DIVISION OF ARRAYS
-
-
-# In[32]:
-
-
-np.power(A,2)  ### WE CAN GET THE POWER OF THE ARRAYS
-
-
-# In[33]:
-
-
-np.power(B,10) ## THE POWER OF B TO 10
-
-
-# In[34]:
-
-
-### WE CAN GET THE POWER OF THE ARRAYS TO THE OTHER
-
-np.power(A,B)
-
-
-# In[35]:
-
-
-### CONDITIONAL STATEMENTS IN NUMPY ARRAY
-
-x = np.array([i for i in range(10)])
-
-print(x)
-
-
-# In[37]:
-
-
-np.where(x%2==0,'Even','Odd') ## code takes in conditions an dprints output if true
-
-
-# In[40]:
-
-
-## create condition list
-condlist = [x<5,x>5]
-choicelist = [x**2,x**3]
-
-
-# In[41]:
-
-
-np.select(condlist,choicelist,default=x)
+np.ceil(arr)
 
 
 # In[ ]:
